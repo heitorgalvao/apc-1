e9b0ce7622c7b5285fbe12213e3afa0615db094d
heitorgalvao <heitorabrantesgalvaodearaujo@gmail.com>
2025-03-19 09:38:12 -0300

    Adicionando arquivos da aula02

---
+++ aulas/aula02/imprimir.caracter.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf( "Ola turma\n");
+    printf("Vamos \t aprender a programar em C");
+    
+return 0;
+}
---
+++ aulas/aula02/imprimir.inteiros.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("%i\n",10000);
+    printf("%i %i", 10000, 20000);
+    return 0;
+}
