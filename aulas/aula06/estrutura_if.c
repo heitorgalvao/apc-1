28aeee3142df5c9f10744f5ab1491b66e55eebe6
heitorgalvao <heitorabrantesgalvaodearaujo@gmail.com>
2025-05-01 15:33:30 -0300

    adicionando pratica02

---
+++ aulas/aula06/estrutura_if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+    int main(){
+        int numero;
+        printf("Digite um número inteiro: ");
+            scanf("%i", &numero);
+            int numero_eh_divisivel_por_2 = numero % 2 == 0;
+                if(numero_eh_divisivel_por_2) {
+                    printf("O numero %i eh par!\n", numero);
+                }else{
+                    printf("O numero %i eh impar!\n", numero);
+                      }
+             return 0;         
+                }
+        
+    
---
+++ praticas/pratica6/desconto.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+        int main(){
+            float valor_bruto; 
+            float valor_desconto;
+
+            printf("insira o valor bruto do produto:");
+            scanf("%f", &valor_bruto);
+            if(valor_bruto<=100.00f);
+            valor_desconto = valor_bruto * 0.01f;
+    
+            return 0;
+        }
