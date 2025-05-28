5fa08079cb1a5864825185f51762b87c445390a3
heitorgalvao <heitorabrantesgalvaodearaujo@gmail.com>
2025-05-28 08:45:26 -0300

    colorindo o codigo

--- praticas/pratica08/nota_fiscal.c
+++ praticas/pratica08/nota_fiscal.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
     int main(){
+         #define GRN "\e[0;32m"
+        #define BLU "\e[0;34m"
+        #define RED "\e[0;31m"
+        #define MAG "\e[0;35m"
+        #define reset "\e[0m"
         float total = 0.0f;
         float nota_fiscal[5][3];
         printf("entre com 5 valores:\n");
@@ -12,9 +17,11 @@
             nota_fiscal[i][2] = nota_fiscal[i][0] * nota_fiscal[i][1];
             total = total + nota_fiscal[i][2];
         }
+        printf(RED);
         printf("\n--- Detalhes da Nota Fiscal ---\n");
         printf("Item | Quantidade | Preco Unit. | Subtotal\n");
         printf("------------------------------------------\n");
+        printf(reset);
         for (int i = 0; i < 5; i++) {
         printf("%4d |", i + 1);
         for (int j = 0; j < 3; j++) {
@@ -22,8 +29,10 @@
         }
         printf("\n");
     }
+    printf(GRN);
      printf("------------------------------------------\n");
      printf("Total Geral: %.2f\n", total);
+     printf(reset);
 
      return 0 ;
     }
